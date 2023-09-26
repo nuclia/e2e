21c5cc50f599cb77088efe4eab3fe3b2c6c2d874
Eric BREHAULT <ebrehault@gmail.com>
2023-09-26 10:42:31 +0200

    e2e for settings page (#6)
    * e2e for settings page
    
    * fix

---
+++ cypress/e2e/1-basic/4-kb-settings.cy.js
@@ -0,0 +1,35 @@
+/// <reference types="cypress" />
+
+import { emptyKb, getAuthHeader, goTo } from '../../support/common';
+
+describe('Change KB settings', () => {
+  const endpoint = `https://stashify.cloud/api/v1/account/testing/kb/${emptyKb.name}`;
+  const authHeader = getAuthHeader();
+
+  before(() => {
+    cy.request({
+      method: 'PATCH',
+      url: `${endpoint}`,
+      headers: authHeader,
+      body: {
+        description: `DO NOT DELETE\nKnowledge box used to test content addition through E2E tests, and should always be empty at the end of the tests`,
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+    });
+  });
+
+  it('should allow to change the kb settings', () => {
+    cy.loginToEmptyKb();
+    goTo('Settings');
+    cy.scrollTo('bottom');
+    cy.contains('Save').should('be.disabled');
+    cy.scrollTo('top');
+    cy.get(`[formcontrolname="description"] textarea`).type(
+      '\nWhy did you say that? Now I feel like I want to delete this kb.\n'
+    );
+    cy.scrollTo('bottom');
+    cy.contains('Save').click();
+    cy.get('.pa-toast-wrapper').should('contain', 'The new settings have been saved successfully');
+  });
+});
