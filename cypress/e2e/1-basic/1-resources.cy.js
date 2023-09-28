93e43b7e08f1734f345d1bd7c30a7920e60052e1
mpellerin42 <mpellerin42@gmail.com>
2023-09-28 12:22:49 +0200

    Check label added in resource list instead of through API

--- cypress/e2e/1-basic/1-resources.cy.js
+++ cypress/e2e/1-basic/1-resources.cy.js
@@ -106,57 +106,40 @@ describe('Resources', () => {
     });
 
     it('should create a label set and classify a resource with it', () => {
-      cy.get('[data-cy="kb-endpoint"]').then($endpointContainer => {
-        const endpoint = $endpointContainer.text().trim();
+      const labelset = 'Heroes';
+      const label = 'Catwoman';
+      cy.task('log', 'Create a label set');
+      goTo('Classification');
+      cy.contains('Add new').click();
+      cy.get('input#title-input').type(labelset);
+      cy.get('.pa-toggle').contains('Resources').click();
+      cy.get('.label-content.unsaved-label input').type(`${label}{enter}`);
+      cy.get('.label-content.unsaved-label input').type('Poison Ivy{enter}');
+      cy.contains('Save').click();
+      cy.get('pa-expander-header').should('contain', 'Heroes');
 
-        cy.task('log', 'Create a label set');
-        goTo('Classification');
-        cy.contains('Add new').click();
-        cy.get('input#title-input').type('Heroes');
-        cy.get('.pa-toggle').contains('Resources').click();
-        cy.get('.label-content.unsaved-label input').type('Catwoman{enter}');
-        cy.get('.label-content.unsaved-label input').type('Poison Ivy{enter}');
+      cy.task('log', 'Set labels on resources');
+      goTo('Resources list');
+      cy.get('[data-cy="resource-title"]').first().invoke('text').then(resourceTitle => {
+        const title = resourceTitle.trim();
+
+        cy.get('[data-cy="menu-button"] button').first().click();
+        cy.get('ul.pa-menu li').contains('Edit').click();
+        cy.get('pa-button[icon="label"]').click();
+        cy.get('nav ul li').contains('Resource').click();
+        cy.contains('Select the labels').click();
+        cy.contains(labelset).click();
+        cy.contains(label).click();
+        cy.get('body').type('{esc}');
+        cy.get('.label-list pa-chip-closeable').should('have.length', 2);
         cy.contains('Save').click();
-        cy.get('pa-expander-header').should('contain', 'Heroes');
-
-        cy.task('log', 'Set labels on resources');
-        goTo('Resources list');
-        cy.get('[data-cy="resource-title"]').first().invoke('text').then(resourceTitle => {
-          const title = resourceTitle.trim();
-
-          cy.get('[data-cy="menu-button"] button').first().click();
-          cy.get('ul.pa-menu li').contains('Edit').click();
-          cy.get('pa-button[icon="label"]').click();
-          cy.get('nav ul li').contains('Resource').click();
-          cy.contains('Select the labels').click();
-          cy.contains('Heroes').click();
-          cy.contains('Catwoman').click();
-          cy.get('body').type('{esc}');
-          cy.get('.label-list pa-chip-closeable').should('have.length', 2);
-          cy.contains('Save').click();
-          cy.get('.pa-toast-wrapper').should('contain', 'Resource saved');
-          cy.request({
-            method: 'GET',
-            url: `${endpoint}/resources`,
-            headers: getAuthHeader()
-          }).then(response => {
-            expect(response.status).to.eq(200);
-
-            response.body['resources'].forEach((resource) => {
-              if (resource.title === title) {
-                expect(resource['usermetadata']['classifications']).to.deep.equal([{
-                  labelset: 'dataset',
-                  label: 'permanent',
-                  cancelled_by_user: false
-                },{
-                  labelset: 'heroes',
-                  label: 'Catwoman',
-                  cancelled_by_user: false
-                }], 'includes the label added');
-              }
-            });
-          });
-        });
+        cy.get('.pa-toast-wrapper').should('contain', 'Resource saved');
+        cy.get('pa-button[data-cy="back-to-resources"]').click();
+        cy.get('pa-table-row').first().as('firstRow');
+        cy.get('@firstRow').contains(title);
+        cy.contains('Displayed columns').click();
+        cy.get('pa-checkbox').contains('Classification').click();
+        cy.get('@firstRow').contains(label);
       });
     });
   });
