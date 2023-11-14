4d93514b6863889d3a9d99ab849e99d9ecc23927
mpellerin42 <mpellerin42@gmail.com>
2023-11-14 11:53:01 +0100

    improve resilience of NUA key creation test
    By checking through the API first if there is only the permanent e2e NUA key, if not we remove the extra keys

--- cypress/e2e/2-nua/1-create-key-with-dashboard.cy.js
+++ cypress/e2e/2-nua/1-create-key-with-dashboard.cy.js
@@ -1,13 +1,33 @@
 /// <reference types="cypress" />
 
-import { goTo, user } from '../../support/common';
+import { getAuthHeader, goTo, user } from '../../support/common';
 
 describe('Create NUA key with the dashboard', () => {
+  before(() => {
+    const authHeader = getAuthHeader();
+    // https://stashify.cloud/api/v1/account/testing/nua_clients
+    cy.request({
+      method: 'GET',
+      url: 'https://stashify.cloud/api/v1/account/testing/nua_clients',
+      headers: authHeader
+    }).then(response => {
+      expect(response.status).to.eq(200);
+      const clients = response.body['clients'] || [];
+      if (clients.length > 1) {
+        clients.filter((client) => client['title'] !== 'e2e').forEach(client => {
+          cy.request({
+            method: 'DELETE',
+            url: `https://stashify.cloud/api/v1/account/testing/nua_client/${client.client_id}`,
+            headers: authHeader
+          }).then(patchResponse => expect(patchResponse.status).to.eq(204));
+        });
+      }
+    });
+  });
+
   beforeEach(() => cy.login());
 
   it('creates and deletes key', () => {
-    // TODO: improve resilience by checking through the API first if there is only the permanent e2e NUA key
-
     goTo('go-to-manage-account');
     goTo('go-to-nua-keys');
     // Create NUA key
