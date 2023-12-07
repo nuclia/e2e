b551a1b766e5400a06136673e22da69904d20bde
Eric BREHAULT <ebrehault@gmail.com>
2023-12-07 15:37:07 +0100

    now auth is done properly, the email is already there so the button is enabled

--- cypress/e2e/2-nua/create-key-with-dashboard.cy.js
+++ cypress/e2e/2-nua/create-key-with-dashboard.cy.js
@@ -34,8 +34,6 @@ describe('Create NUA key with the dashboard', () => {
     cy.get('[data-cy="open-create-nua-key-dialog"]').click();
     cy.get('pa-modal-advanced').should('be.visible');
     cy.get('pa-modal-advanced input[name="title"]').should('be.visible').type('A new key');
-    cy.get('pa-modal-advanced').get('[data-cy="save-nua-client"]').get('button').should('be.disabled');
-    cy.get('pa-modal-advanced input[name="contact"]').type(user.email);
     cy.get('pa-modal-advanced').get('[data-cy="save-nua-client"]').click();
     cy.get('pa-modal-dialog').get('[data-cy="copy-token"]').click();
     cy.get('pa-modal-dialog').get('[data-cy="close-token-dialog"]').click();
