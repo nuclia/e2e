1a9d83c8b12128dc91433e27e746586928736792
Ramon Navarro Bosch <ramon.nb@gmail.com>
2023-12-27 14:00:36 +0100

    Fix (#42)
    * Fix
    
    * relax rag asserts
    
    * Update test_llm_rag.py
    
    * remove references to Eudald and Ramon in Palm test
    
    * anthropic fix
    
    ---------
    
    Co-authored-by: Carmen Iniesta <c.iniesta@gmail.com>

--- nua/e2e/regional/test_llm_generate.py
+++ nua/e2e/regional/test_llm_generate.py
@@ -3,7 +3,9 @@ from nuclia.sdk.predict import NucliaPredict
 
 def test_llm_generate_chatgpt(nua_config):
     np = NucliaPredict()
-    generated = np.generate("Which is the capital of Catalonia?", model="chatgpt")
+    generated = np.generate(
+        "Which is the capital of Catalonia?", model="chatgpt-azure-3"
+    )
     assert b"Barcelona" in generated
 
 
@@ -21,29 +23,7 @@ def test_llm_generate_anthropic(nua_config):
 
 def test_llm_generate_palm(nua_config):
     np = NucliaPredict()
-    generated = np.generate("Which is the capital of Catalonia?", model="palm")
-    assert b"Barcelona" in generated
-
-
-def test_llm_generate_cohere(nua_config):
-    np = NucliaPredict()
-    generated = np.generate("Which is the capital of Catalonia?", model="cohere")
-    assert b"Barcelona" in generated
-
-
-def test_llm_generate_nuclia_atlas_v1(nua_config):
-    np = NucliaPredict()
-    generated = np.generate(
-        "Which is the capital of Catalonia?", model="nuclia-atlas-v1"
-    )
-    assert b"Barcelona" in generated
-
-
-def test_llm_generate_nuclia_etna_v1(nua_config):
-    np = NucliaPredict()
-    generated = np.generate(
-        "Which is the capital of Catalonia?", model="nuclia-etna-v1"
-    )
+    generated = np.generate("Which is the capital of Catalonia?", model="gemini-pro")
     assert b"Barcelona" in generated
 
 
