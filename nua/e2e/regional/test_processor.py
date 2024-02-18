089584002a3459bb7cb311d71d385539ae427818
Ramon Navarro Bosch <ramon@nuclia.com>
2024-02-18 16:45:26 +0100

    Fix

--- nua/e2e/regional/test_processor.py
+++ nua/e2e/regional/test_processor.py
@@ -9,11 +9,11 @@ def define_path(file: str):
     return FILE_PATH + file
 
 
-@pytest.mark.timeout(360)
+@pytest.mark.timeout(660)
 def test_pdf(nua_config):
     path = define_path("2310.14587.pdf")
     nc = NucliaProcessing()
-    payload = nc.process_file(path, kbid="kbid", timeout=200)
+    payload = nc.process_file(path, kbid="kbid", timeout=300)
     assert payload
     assert (
         "As the training data of LLMs often contains undesirable"
@@ -25,7 +25,7 @@ def test_pdf(nua_config):
 def test_video(nua_config):
     path = define_path("simple_video.mp4")
     nc = NucliaProcessing()
-    payload = nc.process_file(path, kbid="kbid", timeout=200)
+    payload = nc.process_file(path, kbid="kbid", timeout=300)
     assert payload
     assert (
         "This is one of the most reflective mirrors"
@@ -33,28 +33,16 @@ def test_video(nua_config):
     )
 
 
-@pytest.mark.timeout(360)
+@pytest.mark.timeout(660)
 def test_vude_1(nua_config):
     path = define_path(
         "y2mate_is_Stone_1_Minute_Short_Film_Hot_Shot_5hPtU8Jbpg0_720p_1701938639.mp4"  # noqa
     )
     nc = NucliaProcessing()
-    payload = nc.process_file(path, kbid="kbid", timeout=200)
+    payload = nc.process_file(path, kbid="kbid", timeout=300)
     assert payload
     print(payload.extracted_text[0].body)
     assert "harmful" in payload.extracted_text[0].body.text
-
-
-@pytest.mark.timeout(360)
-def test_vude_2(nua_config):
-    path = define_path(
-        "yt5s.io-The Wait  - 1 Minute Short Film _ Award Winning.mp4"
-    )  # noqa
-    nc = NucliaProcessing()
-    payload = nc.process_file(path, kbid="kbid", timeout=200)
-    assert payload
-    print(payload.extracted_text[0].body)
-    assert "two months" in payload.extracted_text[0].body.text
 
 
 def test_activity(nua_config):
