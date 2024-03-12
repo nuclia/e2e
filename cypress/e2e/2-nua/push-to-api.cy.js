ead4a0f1bb603deb7aec39ee791e77489bb7e2b3
Mat Pellerin <mpellerin42@gmail.com>
2024-03-12 15:25:10 +0100

    Fix prod NUA keys variables (#76)
    Also use `x-nuclia-nuakey` headers instead of the deprecated `x-stf-nuakey` one.

--- cypress/e2e/2-nua/push-to-api.cy.js
+++ cypress/e2e/2-nua/push-to-api.cy.js
@@ -9,9 +9,9 @@ describe('Push file', () => {
           cy.request({
             method: 'POST',
             url: `https://${zone.slug}.${ACCOUNT.domain}/api/v1/processing/upload`,
-            headers: { 'x-stf-nuakey': `Bearer ${zone.nuaKey}`, md5: 'fa7bfc3072bf547b3d3f5c75050adadf' },
-            body: file
-          })
+            headers: { 'x-nuclia-nuakey': `Bearer ${zone.nuaKey}`, md5: 'fa7bfc3072bf547b3d3f5c75050adadf' },
+            body: file,
+          }),
         )
         .then((resp) => {
           expect(resp.status).to.eq(200);
@@ -19,24 +19,22 @@ describe('Push file', () => {
             method: 'POST',
             json: true,
             url: `https://${zone.slug}.${ACCOUNT.domain}/api/v1/processing/push`,
-            headers: { 'x-stf-nuakey': `Bearer ${zone.nuaKey}` },
+            headers: { 'x-nuclia-nuakey': `Bearer ${zone.nuaKey}` },
             body: {
-              filefield: { 'nuclia-logo.png': resp.body }
-            }
+              filefield: { 'nuclia-logo.png': resp.body },
+            },
           });
         });
-    })
+    });
   });
 
   it('pulls results', () => {
     ACCOUNT.availableZones.forEach((zone) => {
-      cy
-        .request({
-          method: 'GET',
-          url: `https://${zone.slug}.${ACCOUNT.domain}/api/v1/processing/pull`,
-          headers: { 'x-stf-nuakey': `Bearer ${zone.nuaKey}` }
-        })
-        .then((res) => expect(res.status).to.eq(200));
+      cy.request({
+        method: 'GET',
+        url: `https://${zone.slug}.${ACCOUNT.domain}/api/v1/processing/pull`,
+        headers: { 'x-stf-nuakey': `Bearer ${zone.nuaKey}` },
+      }).then((res) => expect(res.status).to.eq(200));
     });
   });
 });
