240567c0086dd3021087baf29f82ac866d689b52
oriol <o.perramon@iskra.cat>
2024-11-05 11:51:45 +0100

    Fix login form selectors

--- cypress/e2e/0-user/auth.cy.js
+++ cypress/e2e/0-user/auth.cy.js
@@ -10,7 +10,7 @@ describe('User Login', () => {
 
   it('should redirect to kb selection page after login', () => {
     cy.visit('/');
-    cy.get(`[formcontrolname="email"] input`).type(user.email, { log: false });
+    cy.get(`[type="email"] input`).type(user.email, { log: false });
     cy.get(`[data-cy="password"] input[type="password"]`).type(user.password, { log: false });
     cy.get(`button[type='submit']`).click();
     cy.location('pathname').should('equal', `/select/${ACCOUNT.slug}`);
@@ -19,7 +19,7 @@ describe('User Login', () => {
   it('should allow a visitor to login and logout', () => {
     const permanentKb = ACCOUNT.availableZones[0].permanentKb;
     cy.visit('/');
-    cy.get(`[formcontrolname="email"] input`).type(user.email, { log: false });
+    cy.get(`[type="email"] input`).type(user.email, { log: false });
     cy.get(`[data-cy="password"] input[type="password"]`).type(`${user.password}{enter}`, { log: false });
     cy.get('.select-kb-list a').contains(permanentKb.name).click();
     cy.location('pathname').should('equal', `/at/${ACCOUNT.slug}/${permanentKb.zone}/${permanentKb.slug}`);
@@ -33,8 +33,8 @@ describe('User Login', () => {
 
   it('should display login errors', () => {
     cy.visit('/');
-    cy.get(`[formcontrolname="email"] input`).type('user').blur().focus().clear().blur();
-    cy.get('[formcontrolname="email"] .pa-field-help-error').should('be.visible').and('contain', 'Required field');
+    cy.get(`[type="email"] input`).type('user').blur().focus().clear().blur();
+    cy.get('[type="email"] .pa-field-help-error').should('be.visible').and('contain', 'Required field');
 
     cy.get(`[data-cy="password"] input[type="password"]`).type('wrong password').clear().blur();
     cy.get('[data-cy="password"] .pa-field-help-error').should('be.visible').and('contain', 'Required field');
@@ -44,7 +44,7 @@ describe('User Login', () => {
 
   it('should error for an invalid user', () => {
     cy.visit('/');
-    cy.get(`[formcontrolname="email"] input`).type('wrong-email@gmail.com', { log: false });
+    cy.get(`[type="email"] input`).type('wrong-email@gmail.com', { log: false });
     cy.get(`[data-cy="password"] input[type="password"]`).type('invalid{enter}', { log: false });
     cy.get(`[data-cy="login-error"]`)
       .should('contain', 'Authentication error.')
@@ -53,7 +53,7 @@ describe('User Login', () => {
 
   it('should error for an invalid password for existing user', () => {
     cy.visit('/');
-    cy.get(`[formcontrolname="email"] input`).type(user.email, { log: false });
+    cy.get(`[type="email"] input`).type(user.email, { log: false });
     cy.get(`[data-cy="password"] input[type="password"]`).type('invalid{enter}', { log: false });
     cy.get(`[data-cy="login-error"]`)
       .should('contain', 'Authentication error.')
