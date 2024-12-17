8c5bdafdf39a1ba8c35e0d2b10fbb146e147a5e7
Eric BREHAULT <ebrehault@gmail.com>
2024-12-17 11:11:23 +0100

    improve test

--- cypress/e2e/5-ui-starter/ui-starter.cy.js
+++ cypress/e2e/5-ui-starter/ui-starter.cy.js
@@ -14,6 +14,10 @@ describe('UI Starter', () => {
     } else {
       cy.visit('http://localhost:4173');
       cy.get('.nuclia-widget .sw-search-input').should('exist');
+      cy.get('.sw-textarea textarea').click();
+      cy.get('.sw-textarea textarea').type(`Gold price evolution in 2023\n`, { force: true });
+      cy.get('.sw-initial-answer').should('exist');
+      cy.get('h3.result-title').should('exist');
     }
   });
 });
