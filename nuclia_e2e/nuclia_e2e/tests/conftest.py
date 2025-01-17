2d84edded73c0e155804c9bcf4cfe1916e494711
Carles Bruguera <carlesba@gmail.com>
2025-01-17 11:11:23 +0100

    fix

--- nuclia_e2e/nuclia_e2e/tests/conftest.py
+++ nuclia_e2e/nuclia_e2e/tests/conftest.py
@@ -78,9 +78,6 @@ CLUSTERS_CONFIG = {
     },
 }
 
-from base64 import b64encode
-print(b64encode(str(os.environ.get("TEST_GMAIL_APP_PASSWORD")).encode()))
-
 class ManagerAPI:
     def __init__(self, global_api, session: aiohttp.ClientSession):
         self.global_api = global_api
