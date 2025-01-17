202f4ba5c595fa8d5ea83dde05ce52ab3538e74f
operramon <52449656+operramon@users.noreply.github.com>
2025-01-17 16:02:20 +0100

    Fix resource preview selector (#134)

--- cypress/e2e/1-basic/resources.cy.js
+++ cypress/e2e/1-basic/resources.cy.js
@@ -80,7 +80,7 @@ describe('Resources', () => {
         it('should allow to preview', () => {
           cy.contains('Lamarr Lesson plan.pdf').click();
           cy.get('.edit-resource > header').should('contain', 'Lamarr Lesson plan.pdf');
-          cy.get('.edit-resource .main-container .paragraph-container').should('contain', 'Lamarr Lesson plan.pdf');
+          cy.get('.edit-resource .main-container .resource-container').should('contain', 'Lamarr Lesson plan.pdf');
           cy.get('.edit-resource nav li:not(.active):first').click();
           cy.get('.edit-resource .main-container').should('contain', 'Hedy Lamarr, An Inventive Mind');
         });
