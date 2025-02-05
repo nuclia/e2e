f572a7611e2de4cc685728fce1469c51c1acc0f8
Joan Antoni RE <joanantoni.re16@gmail.com>
2025-02-05 18:25:21 +0100

    Add more typing and fix small typo-bug (#137)
    * Add install target to makefile
    
    * Add egg-info/ to gitignore
    
    * Improve typing for test_kb_features
    
    * Fix wrong field name for GuardOperation
    
    * More typing
    
    * Add mypy to lint and lint-autofix make targets
    
    * Update test_da_tasks.py
    
    ---------
    
    Co-authored-by: Carles Bruguera Artero <carlesba@gmail.com>

--- nuclia_e2e/nuclia_e2e/tests/test_kb_features.py
+++ nuclia_e2e/nuclia_e2e/tests/test_kb_features.py
@@ -1,4 +1,5 @@
-from collections.abc import Coroutine
+from collections.abc import Awaitable
+from collections.abc import Callable
 from datetime import datetime
 from datetime import timedelta
 from datetime import timezone
@@ -35,6 +36,9 @@ import asyncio
 import backoff
 import pytest
 
+Logger = Callable[[str], None]
+
+
 NUCLIADB_KB_ENDPOINT = "/api/v1/kb/{kb}"
 ASSETS_FILE_PATH = f"{Path(__file__).parent.parent}/assets"
 
@@ -44,7 +48,10 @@ TEST_CHOCO_ASK_MORE = "When did they start being high?"
 
 
 async def wait_for(
-    condition: Coroutine, max_wait: int = 60, interval: int = 5, logger=None
+    condition: Callable[[], Awaitable],
+    max_wait: int = 60,
+    interval: int = 5,
+    logger: Logger = print,
 ) -> tuple[bool, Any]:
     func_name = condition.__name__
     logger(f"start wait_for '{func_name}', max_wait={max_wait}s")
@@ -57,7 +64,7 @@ async def wait_for(
     return success, data
 
 
-async def get_kbid_from_slug(zone: str, slug: str) -> str:
+async def get_kbid_from_slug(zone: str, slug: str) -> str | None:
     kbs = NucliaKBS()
     all_kbs = await asyncio.to_thread(kbs.list)
     kbids_by_slug = {kb.slug: kb.id for kb in all_kbs}
@@ -65,7 +72,9 @@ async def get_kbid_from_slug(zone: str, slug: str) -> str:
     return kbid
 
 
-def get_async_kb_ndb_client(zone, account, kbid, user_token):
+def get_async_kb_ndb_client(
+    zone: str, account: str, kbid: str, user_token: str | None
+) -> AsyncNucliaDBClient:
     from nuclia import REGIONAL
 
     kb_path = NUCLIADB_KB_ENDPOINT.format(zone=zone, account=account, kb=kbid)
@@ -81,7 +90,7 @@ def get_async_kb_ndb_client(zone, account, kbid, user_token):
     return ndb
 
 
-def get_sync_kb_ndb_client(zone, account, kbid, user_token):
+def get_sync_kb_ndb_client(zone: str, account: str, kbid: str, user_token: str | None) -> NucliaDBClient:
     from nuclia import REGIONAL
 
     kb_path = NUCLIADB_KB_ENDPOINT.format(zone=zone, account=account, kb=kbid)
@@ -97,7 +106,7 @@ def get_sync_kb_ndb_client(zone, account, kbid, user_token):
     return ndb
 
 
-async def run_test_kb_creation(regional_api_config, logger) -> str:
+async def run_test_kb_creation(regional_api_config, logger: Logger) -> str:
     kbs = NucliaKBS()
     new_kb = await asyncio.to_thread(
         partial(
@@ -114,7 +123,7 @@ async def run_test_kb_creation(regional_api_config, logger) -> str:
     return kbid
 
 
-async def run_test_upload_and_process(regional_api_config, ndb, logger):
+async def run_test_upload_and_process(regional_api_config, ndb: AsyncNucliaDBClient, logger: Logger):
     kb = AsyncNucliaKB()
     rid = await kb.resource.create(
         title="How this chocolatier is navigating an unexpected spike in cocoa prices",
@@ -158,7 +167,7 @@ async def run_test_upload_and_process(regional_api_config, ndb, logger):
     assert success
 
 
-async def run_test_import_kb(regional_api_config, ndb, logger):
+async def run_test_import_kb(regional_api_config, ndb: AsyncNucliaDBClient, logger: Logger):
     """
     Imports a kb with three resources and some labelsets already created
     """
@@ -185,7 +194,7 @@ async def run_test_import_kb(regional_api_config, ndb, logger):
     assert success
 
 
-async def run_test_create_da_labeller(regional_api_config, ndb, logger):
+async def run_test_create_da_labeller(regional_api_config, ndb: NucliaDBClient, logger: Logger):
     """
     Creates a config to run on all current resources and on all future ones
     """
@@ -232,7 +241,7 @@ async def run_test_create_da_labeller(regional_api_config, ndb, logger):
     )
 
 
-async def run_test_check_da_labeller_output(regional_api_config, ndb, logger):
+async def run_test_check_da_labeller_output(regional_api_config, ndb: NucliaDBClient, logger: Logger):
     kb = NucliaKB()
 
     expected_resource_labels = [
@@ -291,7 +300,7 @@ async def run_test_check_da_labeller_output(regional_api_config, ndb, logger):
 
 
 @backoff.on_exception(backoff.constant, AssertionError, max_tries=5, interval=5)
-async def run_test_find(regional_api_config, ndb, logger):
+async def run_test_find(regional_api_config, ndb: AsyncNucliaDBClient, logger: Logger):
     kb = AsyncNucliaKB()
 
     result = await kb.search.find(
@@ -308,7 +317,7 @@ async def run_test_find(regional_api_config, ndb, logger):
 
 
 @backoff.on_exception(backoff.constant, AssertionError, max_tries=5, interval=5)
-async def run_test_ask(regional_api_config, ndb, logger):
+async def run_test_ask(regional_api_config, ndb: AsyncNucliaDBClient, logger: Logger):
     kb = AsyncNucliaKB()
 
     ask_result = await kb.search.ask(
@@ -366,7 +375,9 @@ async def run_test_activity_log(regional_api_config, ndb, logger):
                     ndb=ndb,
                     type=EventType.CHAT,
                     query=ActivityLogsChatQuery(
-                        year_month=f"{now.year}-{now.month:02}", filters={}, pagination=Pagination(limit=100)
+                        year_month=f"{now.year}-{now.month:02}",
+                        filters={},
+                        pagination=Pagination(limit=100),
                     ),
                 )
             )
@@ -440,7 +451,7 @@ async def run_test_kb_deletion(regional_api_config, kbid, logger):
 
 
 @pytest.mark.asyncio_cooperative
-async def test_kb(request, regional_api_config, clean_kb_test):
+async def test_kb(request: pytest.FixtureRequest, regional_api_config, clean_kb_test):
     """
     Test a chain of operations that simulates a normal use of a knowledgebox, just concentrated
     in time.
