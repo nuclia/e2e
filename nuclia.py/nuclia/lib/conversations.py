c4cde3743cc63aabab2c8ed4af1a0a72641cf386
Carles Bruguera <carlesba@gmail.com>
2025-02-17 17:25:52 +0100

    Remove temporal fork

--- nuclia.py/nuclia/lib/conversations.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-from nucliadb_models import Message
-from pydantic import RootModel
-
-
-class Conversation(RootModel[List[Message]]):
-    pass
--- nuclia.py/nuclia/tests/conftest.py
+++
@@ -1,5 +0,0 @@
-pytest_plugins = [
-    "pytest_docker_fixtures",
-    "nuclia.tests.fixtures",
-    "nucliadb_sdk.tests.fixtures",
-]
