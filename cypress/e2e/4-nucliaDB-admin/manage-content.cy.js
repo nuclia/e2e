f233504e25087372d50c212a9c4456f18d04e4a7
Carles Bruguera Artero <carlesba@gmail.com>
2025-03-12 09:14:41 +0100

    Cleanup kbs after test

--- cypress/e2e/4-nucliaDB-admin/manage-content.cy.js
+++ cypress/e2e/4-nucliaDB-admin/manage-content.cy.js
@@ -10,30 +10,25 @@ describe('NucliaDB admin – Manage content', () => {
     }).then(response => {
       expect(response.status).to.eq(200);
       const kbPayload = { 'slug': `${STANDALONE_KB_NAME}`, 'zone': 'local', 'title': `${STANDALONE_KB_NAME}` };
+
       if (response.body['kbs'].length > 0) {
-        response.body['kbs'].forEach(kb => {
+        Cypress._.each(response.body['kbs'], (kb) => {
           cy.request({
             method: 'DELETE',
             url: `http://localhost:8080/api/v1/kb/${kb['uuid']}`,
             headers: STANDALONE_HEADER
           }).then(deleteResponse => {
             expect(deleteResponse.status).to.eq(200);
-            return cy.request({
-              method: 'POST',
-              url: 'http://localhost:8080/api/v1/kbs',
-              headers: STANDALONE_HEADER,
-              body: kbPayload
-            }).then((creationResponse) => expect(creationResponse.status).to.eq(201));
           });
         });
-      } else {
-        return cy.request({
-          method: 'POST',
-          url: 'http://localhost:8080/api/v1/kbs',
-          headers: STANDALONE_HEADER,
-          body: kbPayload
-        }).then((creationResponse) => expect(creationResponse.status).to.eq(201));
       }
+      
+      return cy.request({
+        method: 'POST',
+        url: 'http://localhost:8080/api/v1/kbs',
+        headers: STANDALONE_HEADER,
+        body: kbPayload
+      }).then((creationResponse) => expect(creationResponse.status).to.eq(201));
     });
   });
 
@@ -50,5 +45,26 @@ describe('NucliaDB admin – Manage content', () => {
     cy.get('app-upload-progress button[aria-label="Close"]').click();
     cy.get('.pa-toast-wrapper').should('contain', 'Upload successful');
     cy.location('hash').should('contain', `/resources/pending`);
+  });
+
+  afterEach(() => {
+    // Cleanup: Delete any KB created for the test
+    cy.request({
+      method: 'GET',
+      url: 'http://localhost:8080/api/v1/kbs',
+      headers: STANDALONE_HEADER
+    }).then(response => {
+      if (response.body['kbs'].length > 0) {
+        Cypress._.each(response.body['kbs'], (kb) => {
+          cy.request({
+            method: 'DELETE',
+            url: `http://localhost:8080/api/v1/kb/${kb['uuid']}`,
+            headers: STANDALONE_HEADER
+          }).then(deleteResponse => {
+            expect(deleteResponse.status).to.eq(200);
+          });
+        });
+      }
+    });
   });
 });
