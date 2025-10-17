1bf54b03fa217162433cbc8be5ed86e32fc26f56
Ferran Llamas <ferran@nuclia.com>
2025-10-17 14:48:55 +0200

    fix

--- nuclia_e2e/nuclia_e2e/tests/utils.py
+++ nuclia_e2e/nuclia_e2e/tests/utils.py
@@ -104,7 +104,7 @@ async def create_omelette_resource(ndb: AsyncNucliaDBClient):
     max_wait_seconds: int = 300
     start = time.time()
     while (time.time() - start) < max_wait_seconds:
-        resource = await kb.resource.get(slug=slug)
+        resource = await kb.resource.get(slug=slug, show=["values"])
         status = resource.data.texts["omelette"].status
         if status == "PROCESSED":
             break
