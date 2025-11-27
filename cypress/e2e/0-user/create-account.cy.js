b3473fd16af35becbc074e02d8ab486b45cf9fff
Eric BREHAULT <ebrehault@gmail.com>
2025-11-27 18:00:25 +0100

    Ericbrehault/sc 13531/set cookie consent on dashboard 4 (#204)
    * Revert "I forgot one (#202)"
    
    This reverts commit 3a9a525a0127dd6d5ed5310ab16d5e6272afb19d.
    
    * Revert "we must accept cookies because e2e are executed outside Europe :) (#201)"
    
    This reverts commit e73c7c88ac0cc4e81b2707018b8778d0679f4ace.
    
    * Revert "cookie banner (#200)"
    
    This reverts commit 1671251003219f599bfa1d0c1d5e6792330c5db3.
    
    * cookie consent on sign up

--- cypress/e2e/0-user/create-account.cy.js
+++ cypress/e2e/0-user/create-account.cy.js
@@ -2,8 +2,9 @@
 
 describe('Create a new account', () => {
   it('allows to go to account creation', () => {
-    cy.nonLoggedVisit('/');
+    cy.visit('/');
     cy.get('[data-cy="create-account"]').click();
+    cy.get('*[aria-label="Cookie banner"] button#onetrust-accept-btn-handler').click();
     cy.get('[data-cy="signup-form"]').should('exist');
     cy.get('button[type="submit"]').should('be.disabled');
     cy.get(`[formcontrolname="name"] input`).type('Bruce Wayne');
