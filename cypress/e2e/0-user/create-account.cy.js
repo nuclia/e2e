1dba776f401ffbf51c9647e45be253196cbbed45
Eric BREHAULT <ebrehault@gmail.com>
2025-11-27 17:49:32 +0100

    cookie consent on sign up

--- cypress/e2e/0-user/create-account.cy.js
+++ cypress/e2e/0-user/create-account.cy.js
@@ -4,6 +4,7 @@ describe('Create a new account', () => {
   it('allows to go to account creation', () => {
     cy.visit('/');
     cy.get('[data-cy="create-account"]').click();
+    cy.get('*[aria-label="Cookie banner"] button#onetrust-accept-btn-handler').click();
     cy.get('[data-cy="signup-form"]').should('exist');
     cy.get('button[type="submit"]').should('be.disabled');
     cy.get(`[formcontrolname="name"] input`).type('Bruce Wayne');
