e3878441ff143368d0016e216c95cd0e3c61b9a7
Eric BREHAULT <ebrehault@gmail.com>
2026-04-15 09:16:16 +0200

    make a POST to initiate sign up (#263)

--- cypress/e2e/0-user/create-account.cy.js
+++ cypress/e2e/0-user/create-account.cy.js
@@ -1,21 +1,30 @@
 /// <reference types="cypress" />
 
+const { ACCOUNT } = require('../../support/common');
+
 describe('Create a new account', () => {
   it('allows to go to account creation', () => {
-    cy.visit('/');
-    cy.get('[data-cy="create-account"]').click();
-    cy.get('*[aria-label="Cookie banner"] button#onetrust-accept-btn-handler').click();
-    cy.get('[data-cy="signup-form"]').should('exist');
-    cy.get(`[name="email"] input`).type('test@not-a-real-email.nuclia.com');
-    cy.get(`[name="fullname"] input`).type('Bruce Wayne');
-    cy.get(`[name="company"] input`).type('DC Comics');
-    cy.get('button[type="submit"]').should('be.enabled');
-    cy.get('button[type="submit"]').click();
-    cy.get('*[aria-label="Cookie banner"] button#onetrust-accept-btn-handler').click();
-    cy.get('button[type="submit"]').should('be.disabled');
-    cy.get(`[formcontrolname="password"] input`).type('Batman12345678!');
-    cy.get('button[type="submit"]').should('be.enabled');
-    cy.get('button[type="submit"]').click();
-    cy.get('[qa="confirmation-title"]').should('contain', 'Check your email inbox!');
+    cy.request({
+      method: 'POST',
+      url: `https://accounts.${ACCOUNT.domain}/api/auth/signup/start`,
+      body: {
+        app: `https://rag.${ACCOUNT.domain}`,
+        email: 'test@not-a-real-email.nuclia.com',
+        fullname: 'Bruce Wayne',
+        company: 'DC Comics',
+      },
+      form: true,
+      followRedirect: false,
+    }).then((response) => {
+      const redirect = response.headers.location;
+      expect(redirect.startsWith(`https://rag.${ACCOUNT.domain}`)).to.eq(true);
+      cy.visit(redirect);
+      cy.get('*[aria-label="Cookie banner"] button#onetrust-accept-btn-handler').click();
+      cy.get('button[type="submit"]').should('be.disabled');
+      cy.get(`[formcontrolname="password"] input`).type('Batman12345678!');
+      cy.get('button[type="submit"]').should('be.enabled');
+      cy.get('button[type="submit"]').click();
+      cy.get('[qa="confirmation-title"]').should('contain', 'Check your email inbox!');
+    });
   });
 });
